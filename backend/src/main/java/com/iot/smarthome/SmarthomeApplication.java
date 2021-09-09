11add105b58e1e6ef041219adf7d56137bbda9b1
Georgi Velev <39666872+ggvelev@users.noreply.github.com>
2021-09-09 19:38:05 +0300

    Restructure project

--- backend/src/main/java/com/iot/smarthome/SmarthomeApplication.java
+++ backend/src/main/java/com/iot/smarthome/SmarthomeApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SmarthomeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SmarthomeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SmarthomeApplication.class, args);
+    }
 
 }
