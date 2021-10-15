2e659c91bd54e010f78803e817e74ca4835a9385
Georgi Velev <39666872+ggvelev@users.noreply.github.com>
2021-10-15 20:19:32 +0300

    Add 'existsBy' UserRepository query methods

--- backend/src/main/java/com/iot/smarthome/repository/UserRepository.java
+++ backend/src/main/java/com/iot/smarthome/repository/UserRepository.java
@@ -45,4 +45,7 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
     Optional<UserEntity> findByUsername(String username);
 
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
 }
