1b60569aa66c99696599e5dbd6bfe3f1b7a5bb4d
Georgi Velev <39666872+ggvelev@users.noreply.github.com>
2021-10-18 16:01:18 +0300

    Add ErrorResponse definition

---
+++ backend/src/main/java/com/iot/smarthome/dto/ErrorResponse.java
@@ -0,0 +1,85 @@
+/*******************************************************************************
+ * MIT License
+ *
+ * Copyright (c) 2021 Georgi Velev
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ ******************************************************************************/
+
+package com.iot.smarthome.dto;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Generic error response details (mainly returned by REST APIs)
+ */
+public class ErrorResponse {
+
+    private final String error;
+    private final String message;
+
+    @JsonCreator
+    public ErrorResponse(@JsonProperty("error") String error,
+                         @JsonProperty("message") String message) {
+        this.error = error;
+        this.message = message;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        ErrorResponse that = (ErrorResponse) o;
+
+        if (!error.equals(that.error)) {
+            return false;
+        }
+        return message.equals(that.message);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = error.hashCode();
+        result = 31 * result + message.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "ErrorResponse{" +
+                "error='" + error + '\'' +
+                ", message='" + message + '\'' +
+                '}';
+    }
+}
