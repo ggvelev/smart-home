d18f4260921a7c29d851fa66d703e8136d1cbbbb
Georgi Velev <39666872+ggvelev@users.noreply.github.com>
2021-10-25 11:51:36 +0300

    [GH-14] Add initial configuration for OpenAPI UI

---
+++ backend/src/main/java/com/iot/smarthome/config/OpenApiConfig.java
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * MIT License
+ *
+ * Copyright (c) 2021 Georgi Velev
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ ******************************************************************************/
+
+package com.iot.smarthome.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Smart Home API",
+                version = "v1.0",
+                description = "API server for smart-home application",
+                contact = @Contact(name = "Georgi Velev", url = "https://github.com/ggvelev")),
+        security = @SecurityRequirement(name = OpenApiConfig.BEARER_AUTH))
+@SecurityScheme(
+        name = OpenApiConfig.BEARER_AUTH,
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
+public class OpenApiConfig {
+
+    public static final String BEARER_AUTH = "bearerAuth";
+
+}
